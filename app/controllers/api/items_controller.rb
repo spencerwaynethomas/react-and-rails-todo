9435d9e7d4b5fc963ab3d652940383921a26d79f
Spencer Thomas <spencerthomas@Spencers-MacBook-Air.local>
2020-08-04 16:03:57 -0600

    rails backend

---
+++ app/controllers/api/items_controller.rb
@@ -0,0 +1,32 @@
+class Api::ItemsController < ApplicationController
+  def index
+    render json: item.all
+  end
+
+  def create
+    item = Item.new(item_params)
+    if item.save
+      render json: item
+    else
+      render json: { errors: item.errors }, status: :unprocessable_entity
+    end 
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(complete: !item.complete)
+    render:json: item
+  end
+
+  def destroy
+    Item.find(params[:id]).destroy
+    render json: { message: 'Item deleted' }
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :complete)
+  end
+
+end
